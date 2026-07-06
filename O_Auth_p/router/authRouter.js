573433a6df155baf3bb8808e10f5751cb033c28e
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-07-06 11:38:36 +0530

    O Auth code added

---
+++ O_Auth_p/router/authRouter.js
@@ -0,0 +1,33 @@
+
+import express from "express";
+import passport from "passport";
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+
+router.get("/google/login", passport.authenticate("google", { scope: [["email"], ["profile"]] }))
+
+router.get(
+  "/google/redirect",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
+
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      next(new HttpError("failed to logOut"));
+    }
+  });
+
+  res.redirect("/");
+});
+
+
+
+export default router;
