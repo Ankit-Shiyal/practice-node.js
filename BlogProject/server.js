78b2f1c54ec067dd165225f1ccea1f1722ca74b4
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-07-25 11:19:10 +0530

    some change added

--- BlogProject/server.js
+++ BlogProject/server.js
@@ -62,3 +62,31 @@ async function serverStart() {
 }
 
 serverStart();
+
+
+
+
+
+
+// async function serverStart() {
+//   try {
+//     const connect = await connectDB();
+
+//     if (!connect) {
+//       return console.log("failed to connect db");
+//     }
+
+//     app.listen(port, (err) => {
+//       if (err) {
+//         return console.log(err.message);
+//       }
+
+//       console.log(`server listening on port ${port}`);
+//     });
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1);
+//   }
+// }
+
+// serverStart();
