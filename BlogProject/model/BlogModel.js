d3b84475dbf2ef35e71146d2c629abc2d3710e65
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-07-25 13:05:26 +0530

    blog model added

---
+++ BlogProject/model/BlogModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const newBlog = new mongoose.Schema({
+  BlogTitle: {
+    type: String,
+    required: true,
+  },
+  Content: {
+    type: String,
+    required: true,
+  },
+  Category: {
+    type: String,
+    enum: ["Technology", "Sports", "Politics"],
+    required: true,
+  },
+  BlogImg: {
+    type: String,
+  },
+  Cloudinary_Id: {
+    type: String,
+  },
+  Author:{
+    type:mongoose.Schema.type.ObjectId
+  }
+});
+
+
+const BlogSchema = mongoose.model("blog", newBlog);
+
+export default BlogSchema;
