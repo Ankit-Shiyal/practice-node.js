757d24656d9768eafdabb43bda268c9edb75d541
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-07-25 11:17:29 +0530

    blog project jwt authentication added

---
+++ BlogProject/server.js
@@ -0,0 +1,64 @@
+
+import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config({path:"./.env"})
+
+import HttpError from "./middleware/HttpError.js";
+import connectDB from "./config/db.js";
+
+
+import userRouter from "./router/UserRouter.js" 
+import adminRouter from "./router/adminRouter.js"
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use("/user",userRouter)
+app.use("/admin",adminRouter)
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "hello from server" });
+});
+
+app.use((req, res, next) => {
+    return next(new HttpError("requested route not found", 404));
+});
+
+app.use((error, req, res, next) => {
+    if (res.headersSent) {
+        return next(error);
+    }
+
+    res
+        .status(error.statusCode || 500)
+        .json({ message: error.message || "internal server error " });
+});
+
+const port = 5000
+
+async function serverStart() {
+  try {
+    const connect = await connectDB();
+
+    if (!connect) {
+      return console.log("failed to connect db");
+    }
+
+    app.listen(port, (err) => {
+      if (err) {
+        return console.log(err.message);
+      }
+
+      console.log(`server listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+}
+
+serverStart();
