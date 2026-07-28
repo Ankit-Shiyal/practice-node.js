ee5c1019abd6c90bd0d40695193e22a5cdd62c1d
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-07-28 11:23:37 +0530

    blog CRUD opration and balidation added

--- BlogProject/controller/BlogController.js
+++ BlogProject/controller/BlogController.js
@@ -23,7 +23,6 @@ const BlogAdd = async (req, res, next) => {
   }
 };
 
-
 const deleteBlog = async (req, res, next) => {
   try {
     const targetedUser = req.params.id;
@@ -44,4 +43,64 @@ const deleteBlog = async (req, res, next) => {
   }
 };
 
-export default { BlogAdd, deleteBlog };
+const updateBlog = async (req, res, next) => {
+  try {
+    const blog = await BlogModel.findById(req.params.id);
+
+    if (!blog) {
+      return next(new HttpError("Blog not found", 404));
+    }
+
+    const updates = Object.keys(req.body);
+
+    const allowedFields = ["BlogTitle", "Content", "Category"];
+
+    const isValidUpdate = updates.every((field) =>
+      allowedFields.includes(field),
+    );
+
+    if (!isValidUpdate) {
+      return next(new HttpError("Only allowed fields can be updated", 400));
+    }
+
+    if (req.file) {
+      if (blog.Cloudinary_Id) {
+        await cloudinary.uploader.destroy(blog.Cloudinary_Id);
+      }
+
+      blog.BlogImg = req.file.path;
+      blog.Cloudinary_Id = req.file.filename;
+    }
+
+    updates.forEach((field) => {
+      blog[field] = req.body[field];
+    });
+
+    await blog.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Blog updated successfully",
+      blog,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+const getAllBlogs = async (req, res, next) => {
+  try {
+    const blogs = await BlogModel.find();
+
+    res.status(200).json({
+      success: true,
+      message: "All blogs fetched successfully",
+      count: blogs.length,
+      blogs,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+export default { BlogAdd, deleteBlog, updateBlog, getAllBlogs };
--- BlogProject/router/BlogRouter.js
+++ BlogProject/router/BlogRouter.js
@@ -1,15 +1,40 @@
 import express from "express";
 
 import BlogController from "../controller/BlogController.js";
-import auth from "../middleware/auth.js"
+import auth from "../middleware/auth.js";
 import upload from "../middleware/upload.js";
-import checkRole from "../middleware/checkRole.js"
+import checkRole from "../middleware/checkRole.js";
 
+import { addBlogSchema, updateBlogSchema } from "../validation/BlogSchema.js";
 
 const router = express.Router();
 
-router.post("/add", auth, upload.single("BlogImg"), BlogController.BlogAdd);
+router.post(
+  "/add",
+  auth,
+  upload.single("BlogImg"),
+  validate(addBlogSchema),
+  BlogController.BlogAdd,
+);
+router.delete(
+  "/delete/:id",
+  auth,
+  checkRole("user", "admin"),
+  BlogController.deleteBlog,
+);
 
-router.delete("/delete/:id", auth, checkRole("user", "admin"),BlogController.deleteBlog)
+router.patch(
+  "/update/:id",
+  auth,
+  upload.single("BlogImg"),
+  validate(updateBlogSchema),
+  BlogController.updateBlog
+);
+router.get(
+  "/allBlog",
+  auth,
+  checkRole("user", "admin"),
+  BlogController.getAllBlogs,
+);
 
-export default router
+export default router;
