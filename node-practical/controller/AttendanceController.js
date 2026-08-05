9301f5098d72e92ca8020fc981a068d783f07389
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-08-05 10:20:52 +0530

    rilationship added

--- node-practical/controller/AttendanceController.js
+++ node-practical/controller/AttendanceController.js
@@ -1,4 +1,4 @@
-import modelAttendance from "../model/attendanceModel.js"
+import modelAttendance from "../model/attendanceModel.js";
 import HttpError from "../middleware/HttpError.js";
 
 const markAttendance = async (req, res, next) => {
@@ -12,6 +12,9 @@ const markAttendance = async (req, res, next) => {
     });
     await attendance.save();
 
+    await attendance.populate("employeeName", "Name Email");
+    await attendance.populate("markedBy", "Name Email");
+
     res.status(201).json({
       success: true,
       message: "Attendance marked successfully",
@@ -21,23 +24,24 @@ const markAttendance = async (req, res, next) => {
     next(new HttpError(error.message, 500));
   }
 };
-
 const TodayAttendance = async (req, res, next) => {
   try {
-    
     const start = new Date();
     start.setHours(0, 0, 0, 0);
 
     const end = new Date();
     end.setHours(23, 59, 59, 999);
 
+    const attendance = await modelAttendance
+      .find({
+        date: {
+          $gte: start,
+          $lte: end,
+        },
+      })
+      .populate("employeeName", "Name Email Phone Role")
+      .populate("markedBy", "Name Email Phone Role");
 
-    const attendance = await modelAttendance.find({
-      date: {
-        $gte: start,
-        $lte: end,
-      },
-    })
     res.status(200).json({
       success: true,
       attendance,
