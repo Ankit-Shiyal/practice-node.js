ec3c49e2412014d4d22f98f26e2f11f0920441d6
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-08-12 12:37:08 +0530

    category controller and validation added

--- 13-Food_Order/router/categoryRouter.js
+++ 13-Food_Order/router/categoryRouter.js
@@ -1,14 +1,42 @@
-
 import express from "express";
 
 import categoryController from "../controller/categoryController.js";
-import{categoryImage}from "../middleware/upload.js"
+import { categoryImage } from "../middleware/upload.js";
 import auth from "../middleware/auth.js";
 import checkRole from "../middleware/checkRole.js";
+import validate from "../middleware/validate.js"
+import {addCategorySchema,updateCategorySchema} from "../validation/categorySchema.js"
 
 const router = express.Router();
 
-router.post("/addCategory", auth ,categoryImage.single("categoryImage"),checkRole("admin"),categoryController.addCategory )
+router.post(
+  "/addCategory",
+  auth,validate(addCategorySchema),
+  categoryImage.single("categoryImage"),
+  checkRole("admin"),
+  categoryController.addCategory,
+);
 
+router.get(
+  "/allCategory",
+  auth,
+  checkRole("admin"),
+  categoryController.getAllCategory,
+);
 
-export default router
+router.delete(
+  "/delete/:id",
+  auth,
+  checkRole("admin"),
+  categoryController.deleteCategory,
+);
+
+router.patch(
+  "/update/:id",
+  auth,validate(updateCategorySchema),
+  categoryImage.single("categoryImage"),
+  checkRole("admin"),
+  categoryController.updateCategory,
+);
+
+export default router;
