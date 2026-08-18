d5818097288d888b8ba08a7706d590d862edf284
Ankit Shiyal <ankitshiyal2005@gmail.com>
2026-08-18 12:12:22 +0530

    order add and delete code added

--- 13-Food_Order/controller/orderController.js
+++ 13-Food_Order/controller/orderController.js
@@ -14,23 +14,77 @@ const addOrder = async (req, res, next) => {
       _id: { $in: foodIds },
     });
 
+    let totalAmount = 0;
+
+    const orderItems = items.map((item) => {
+      const foodFound = foods.find(
+        (food) => food._id.toString() === item.food.toString(),
+      );
+      const itemsTotal = foodFound.price * item.quantity;
+      totalAmount += itemsTotal;
+      return {
+        food: foodFound._id,
+        quantity: item.quantity,
+        price: foodFound.price,
+        itemTotal: foodFound.price * item.quantity,
+      };
+    });
+
     const order = await orderModel.create({
       customerName: userId,
       restaurant,
-      items,
+      items: orderItems,
       totalAmount,
       deliveryAddress,
       phone,
     });
 
+    const orderPopulate = await order.populate([
+  {
+    path: "customerName",
+    select: "Name Email Phone -_id",
+  },
+  {
+    path: "restaurant",
+    select: "RestaurantName Address Phone -_id",
+  },
+  {
+    path: "items.food",
+    select: "name price description -_id",
+  },
+]);
+
     return res.status(201).json({
       success: true,
       message: "Order placed successfully",
       order,
+       order: orderPopulate,
     });
   } catch (error) {
     return next(new HttpError(error.message));
   }
 };
 
-export default { addOrder };
+
+const deleteOrder = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    const { id } = req.params;
+
+    const order = await orderModel.findOneAndDelete({ _id:id});
+
+    if (!order) {
+      return next(new HttpError("Order not found", 404));
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Order deleted successfully",
+    });
+  } catch (error) {
+    return next(new HttpError(error.message));
+  }
+};
+
+export default { addOrder,deleteOrder };
--- 13-Food_Order/router/orderRouter.js
+++ 13-Food_Order/router/orderRouter.js
@@ -4,10 +4,12 @@ import auth from "../middleware/auth.js";
 import checkRole from "../middleware/checkRole.js";
 import validate from "../middleware/validate.js";
 
-import orderController from  "../controller/orderController.js"
+import orderController from "../controller/orderController.js";
 
 const router = express.Router();
 
-router.post("/add", auth, orderController.addOrder)
+router.post("/add", auth, orderController.addOrder);
 
-export default router
+router.delete("/delete/:id", auth, orderController.deleteOrder);
+
+export default router;
